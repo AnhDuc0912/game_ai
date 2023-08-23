743789b4536910f49d78df34bc68a5eb0bb9dfc9
Võ Dương Khang <114740029+duokhang1676@users.noreply.github.com>
2023-08-23 13:56:07 +0700

    update bfs, dfs (new)

--- src/algorithms/bfs.js
+++ src/algorithms/bfs.js
@@ -1,51 +1,37 @@
-// Returns all nodes in the order in which they were visited.
-// Make nodes point back to their previous node so that we can compute the shortest path
-// by backtracking from the finish node.
-
 export function bfs(grid, startNode, finishNode) {
   const visitedNodesInOrder = [];
-  let nextNodesStack = [startNode];
-  while (nextNodesStack.length) {
-    const currentNode = nextNodesStack.shift();
+  const queue = [];
+  queue.push(startNode);
+
+  while (queue.length !== 0) {
+    const currentNode = queue.shift();
     if (currentNode === finishNode) return visitedNodesInOrder;
 
-    if (
-      !currentNode.isWall &&
-      (currentNode.isStart || !currentNode.isVisited)
-    ) {
+    if (!currentNode.isWall && (currentNode.isStart || !currentNode.isVisited)) {
       currentNode.isVisited = true;
       visitedNodesInOrder.push(currentNode);
-      const {col, row} = currentNode;
-      let nextNode;
-      if (row > 0) {
-        nextNode = grid[row - 1][col];
-        if (!nextNode.isVisited) {
-          nextNode.previousNode = currentNode;
-          nextNodesStack.push(nextNode);
-        }
-      }
-      if (row < grid.length - 1) {
-        nextNode = grid[row + 1][col];
-        if (!nextNode.isVisited) {
-          nextNode.previousNode = currentNode;
-          nextNodesStack.push(nextNode);
-        }
-      }
-      if (col > 0) {
-        nextNode = grid[row][col - 1];
-        if (!nextNode.isVisited) {
-          nextNode.previousNode = currentNode;
-          nextNodesStack.push(nextNode);
-        }
-      }
-      if (col < grid[0].length - 1) {
-        nextNode = grid[row][col + 1];
-        if (!nextNode.isVisited) {
-          nextNode.previousNode = currentNode;
-          nextNodesStack.push(nextNode);
+      const { col, row } = currentNode;
+
+      const neighbors = [
+        { row: row - 1, col: col }, // North
+        { row: row + 1, col: col }, // South
+        { row: row, col: col - 1 }, // West
+        { row: row, col: col + 1 }  // East
+      ];
+
+      for (const neighbor of neighbors) {
+        const { row: newRow, col: newCol } = neighbor;
+        if (
+          newRow >= 0 && newRow < grid.length &&
+          newCol >= 0 && newCol < grid[0].length &&
+          !grid[newRow][newCol].isVisited
+        ) {
+          grid[newRow][newCol].previousNode = currentNode;
+          queue.push(grid[newRow][newCol]);
         }
       }
     }
   }
-  // return visitedNodesInOrder;
+
+  return visitedNodesInOrder;
 }
--- src/algorithms/dfs.js
+++ src/algorithms/dfs.js
@@ -1,55 +1,36 @@
-// Returns all nodes in the order in which they were visited.
-// Make nodes point back to their previous node so that we can compute the shortest path
-// by backtracking from the finish node.
-
 export function dfs(grid, startNode, finishNode) {
-  const visitedNodesInOrder = [];
-  const nextNodesStack = [];
-  nextNodesStack.push(startNode);
-  while (nextNodesStack.length) {
-    const currentNode = nextNodesStack.pop();
+  const visitedNodesInOrder = [];//danh sách các node đã thăm
+  const stack = [];
+  stack.push(startNode);
 
-    if (currentNode === finishNode) {
-      return visitedNodesInOrder;
-    }
+  while (stack.length !== 0) {//Chạy đến hết stack
+    const currentNode = stack.pop();//Lấy ra node hiện tại
+    if (currentNode === finishNode) return visitedNodesInOrder;//return khi tìm thấy node end
+    if (!currentNode.isWall && (!currentNode.isVisited)) {//Node hiện tại không phải là tường và là node chưa thăm
+      currentNode.isVisited = true;//đánh dấu đã thăm
+      visitedNodesInOrder.push(currentNode);//thêm vào danh sách các node đã thăm
+      const { row, col } = currentNode;//lấy hàng và cột của node hiện tại
 
-    if (
-      !currentNode.isWall &&
-      (currentNode.isStart || !currentNode.isVisited)
-    ) {
-      currentNode.isVisited = true;
-      visitedNodesInOrder.push(currentNode);
+      const neighbors = [//Các code kề với nó trên ma trận 
+        { row: row - 1, col: col }, // North
+        { row: row + 1, col: col }, // South
+        { row: row, col: col - 1 }, // West
+        { row: row, col: col + 1 }  // East
+      ];//Bị ngược ???
 
-      const {col, row} = currentNode;
-      let nextNode;
-      if (row > 0) {
-        nextNode = grid[row - 1][col];
-        if (!nextNode.isVisited) {
-          nextNode.previousNode = currentNode;
-          nextNodesStack.push(nextNode);
-        }
-      }
-      if (row < grid.length - 1) {
-        nextNode = grid[row + 1][col];
-        if (!nextNode.isVisited) {
-          nextNode.previousNode = currentNode;
-          nextNodesStack.push(nextNode);
-        }
-      }
-      if (col > 0) {
-        nextNode = grid[row][col - 1];
-        if (!nextNode.isVisited) {
-          nextNode.previousNode = currentNode;
-          nextNodesStack.push(nextNode);
-        }
-      }
-      if (col < grid[0].length - 1) {
-        nextNode = grid[row][col + 1];
-        if (!nextNode.isVisited) {
-          nextNode.previousNode = currentNode;
-          nextNodesStack.push(nextNode);
+      for (const neighbor of neighbors) {//Lấy lần lược từng node kề với node hiện tại
+        const { row: newRow, col: newCol } = neighbor;
+        if (//Thỏa điều kiện là node nằm trong ma trận và chưa được thăm
+          newRow >= 0 && newRow < grid.length &&
+          newCol >= 0 && newCol < grid[0].length &&
+          !grid[newRow][newCol].isVisited
+        ) {
+          grid[newRow][newCol].previousNode = currentNode;//lưu node phía trước của node đang đứng
+          //là node hiên tại phía bên trên vòng lặp, dùng để tìm đường đến node end (đường màu vàng)
+          stack.push(grid[newRow][newCol]);//thêm vào ngăn xếp
         }
       }
     }
   }
+  return visitedNodesInOrder;//trả về danh sách các node đã thăm theo thứ tự.....
 }
